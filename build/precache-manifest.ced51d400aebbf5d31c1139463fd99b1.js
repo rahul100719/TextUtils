bc3593239665c1c7497fe6f90a1737a4d7ca7d12
Rahul <46256465+rahul100719@users.noreply.github.com>
2022-10-20 15:23:09 +0530

    Commit

---
+++ build/precache-manifest.ced51d400aebbf5d31c1139463fd99b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b646d8dcc24639f1a07e",
+    "url": "/static/css/main.8852f933.chunk.css"
+  },
+  {
+    "revision": "b646d8dcc24639f1a07e",
+    "url": "/static/js/main.b646d8dc.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "64824627424b90883149",
+    "url": "/static/js/2.64824627.chunk.js"
+  },
+  {
+    "revision": "9f01a62a234a6fc127f8",
+    "url": "/static/js/runtime~main.9f01a62a.js"
+  },
+  {
+    "revision": "77373aa7411d46b37985c02593587fed",
+    "url": "/index.html"
+  }
+];
